35e715835a7b84285cd980c4b8b082d24be11513
Stepess <stepanersh@gmail.com>
2019-10-05 17:02:26 +0300

    refactor

--- main.py
+++ main.py
@@ -2,66 +2,73 @@
 import numpy as np
 
 
-# minimize the "sum of squared errors".
-# This is how we calculate and correct our error
-def compute_error_for_line_given_points(b, m, x_arr, y_arr):
-    total_error = 0  # sum of square error formula
-    for i in range(0, len(x_arr)):
-        x = x_arr[i]
-        y = y_arr[i]
-        total_error += (y - (m * x + b)) ** 2
-    return total_error / float(len(x_arr))
+def lost_function(a, b, x_arr, y_arr):
+    """
+    calculates lost function based as sum of squared errors
+    we need to minimize the "sum of squared errors"
+    this is to see how far we from correct value
+    :return: total error
+    """
+    return np.sum((y_arr - (a * x_arr + b)) ** 2) / float(len(x_arr))
 
 
-def step_gradient(b_current, m_current, x_arr, y_arr, learning_rate):
-    # gradient descent
-    b_gradient = 0
-    m_gradient = 0
+def step_gradient(a_current, b_current, x_arr, y_arr, learning_rate):
+    """
+    on step of gradient descent
+    :return: new value of a and b
+    """
     num_of_points = float(len(x_arr))
-    for i in range(0, len(x_arr)):
-        x = x_arr[i]
-        y = y_arr[i]
-        b_gradient += -(2 / num_of_points) * (y - (m_current * x + b_current))
-        m_gradient += -(2 / num_of_points) * x * (y - (m_current * x + b_current))
+
+    # Partial derivative of lost function by a
+    a_gradient = np.sum(-(2 / num_of_points) * x_arr * (y_arr - (a_current * x_arr + b_current)))
+    # Partial derivative of lost function by b
+    b_gradient = np.sum(-(2 / num_of_points) * (y_arr - (a_current * x_arr + b_current)))
+
+    new_a = a_current - (learning_rate * a_gradient)
     new_b = b_current - (learning_rate * b_gradient)
-    new_m = m_current - (learning_rate * m_gradient)
-    return [new_b, new_m]
+
+    return [new_a, new_b]
 
 
-def gradient_descent_runner(x_arr, y_arr, starting_b, starting_m, learning_rate, num_iterations):
+def gradient_descent_runner(x_arr, y_arr, starting_a, starting_b, learning_rate, num_iterations):
+    a = starting_a
     b = starting_b
-    m = starting_m
+
     for i in range(num_iterations):
-        b, m = step_gradient(b, m, x_arr, y_arr, learning_rate)
-    return [b, m]
+        a, b = step_gradient(a, b, x_arr, y_arr, learning_rate)
+
+    return [a, b]
 
 
-def run():
-    # Step 1: Collect the data
-    x_arr = np.random.rand(100)
-    print(x_arr)
-
-    a = 4
-    b = 3
-
-    rand_arr = np.random.random_sample(100)
-    y_arr = a * x_arr + b + rand_arr
-    # Step 2: Define our Hyperparameters
-    learning_rate = 0.01  # how fast the data converge
-    # y=mx+b (Slope formule)
-    initial_b = 0  # initial y-intercept guess
-    initial_m = 0  # initial slope guess
-    iter_num = 100000
-    print("Starting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m,
-                                                                              compute_error_for_line_given_points(
-                                                                                  initial_b, initial_m, x_arr, y_arr)))
-    print("Running...")
-    [b, m] = gradient_descent_runner(x_arr, y_arr, initial_b, initial_m, learning_rate, iter_num)
-    print("After {0} iterations b = {1}, m = {2}, error = {3}".format(iter_num, b, m,
-                                                                      compute_error_for_line_given_points(b, m,
-                                                                                                          x_arr,y_arr)))
-
-
-# main function
 if __name__ == "__main__":
-    run()
+    # init sample
+    sample_size = 1000
+
+    x_sample = np.random.rand(sample_size)
+
+    a_real = 2.5
+    b_real = 1.6
+
+    rand_arr = np.random.random_sample(sample_size)
+    y_sample = a_real * x_sample + b_real + rand_arr
+
+    # hyperparameters
+    lr = 0.01  # how fast the data converge
+
+    # initial guesses
+    initial_a = 0
+    initial_b = 0
+
+    iter_num = 10000
+
+    print("Starting gradient descent at a = {0}, b = {1}, error = {2}"
+          .format(initial_a, initial_b,
+                  lost_function(initial_a, initial_b, x_sample, y_sample)))
+
+    print("Running...")
+
+    [a_found, b_found] = gradient_descent_runner(x_sample, y_sample, initial_a, initial_b, lr, iter_num)
+
+    print("After {0} iterations a = {1}, b = {2}, error = {3}"
+          .format(iter_num, a_found, b_found,
+                  lost_function(a_found, b_found, x_sample, y_sample)))
